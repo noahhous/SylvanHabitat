9af78cfe83caed3f634929ad9dfe37e9fd786f0a
noahhous <69410991+noahhous@users.noreply.github.com>
2020-11-27 20:59:07 -0500

    first try

---
+++ qgis2web_2020_11_27-20_52_45_792589/data/CelebrationTrail_15.js
@@ -0,0 +1,8 @@
+var json_CelebrationTrail_15 = {
+"type": "FeatureCollection",
+"name": "CelebrationTrail_15",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
